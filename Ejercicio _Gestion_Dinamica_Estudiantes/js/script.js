cb959cf6b35f365d37e7be4b6903f9f65c74ce1f
André <mrdresmanu@gmail.com>
2025-01-30 20:08:07 +0100

    probando en casa eror funcion add Estudiante en el ejercicio Gestion Dinámica

--- Ejercicio _Gestion_Dinamica_Estudiantes/js/script.js
+++ Ejercicio _Gestion_Dinamica_Estudiantes/js/script.js
@@ -1,1 +1,16 @@
+
 let estudiantes = ["Rubén", "Yoli", "André"];
+let estudiantes2 = ["Ricardo", "Marina"];
+const listaEstudiantes = estudiantes.concat(estudiantes2);
+document.addEventListener("DOMContentLoaded", () => {
+    const addStudentBtn = document.getElementById("btnAddStudent");
+    const btnRemoveStudent = document.getElementById("btnRemoveStudent");
+    let nuevoEstudiante = document.getElementById("nuevoAlumno");
+
+    btnAddStudent.onclick = function () {
+
+        listaEstudiantes.push(nuevoEstudiante.value);
+        alert(listaEstudiantes);
+    }
+});
+
