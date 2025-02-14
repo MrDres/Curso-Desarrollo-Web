d6baf091675c71a96ebb7aa707d48a0a3ff3192e
André <mrdresmanu@gmail.com>
2025-02-14 23:41:09 +0100

    Mejoras de accesibilidad

--- 14022025_ejercicioClassroom_Accesibilidad/js/script.js
+++ 14022025_ejercicioClassroom_Accesibilidad/js/script.js
@@ -1,30 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const contrastButton = document.getElementById("toggle-contrast");
-    const increaseFontButton = document.getElementById("increase-font");
-    const decreaseFontButton = document.getElementById("decrease-font");
-    const body = document.body;
-    const form = document.getElementById("contact-form");
-    const feedback = document.getElementById("feedback");
-
-    //contrastButton.addEventListener('click', highContrast());
-
-    //function highContrast(){
-    //    body.classList.add('high-contrast');
-    //};
-
-    contrastButton.addEventListener("click", () => {
-        body.classList.toggle("high-contrast");
-    });
-    //function incrementarFuente(){
-    //    body.fontsize
-    //}
-    increaseFontButton.addEventListener("click", ()=>{
-        let tamañoLetra=16;
-        tamañoLetra+=2;
-        document.documentElement.style.fontSize = `${tamañoLetra}rem`;
-    });
-
-
-
-
+  const contrastButton = document.getElementById("toggle-contrast");
+  const increaseFontButton = document.getElementById("increase-font");
+  const decreaseFontButton = document.getElementById("decrease-font");
+  const body = document.body;
+  const form = document.getElementById("contact-form");
+  const feedback = document.getElementById("feedback");
+  const maxfontsize = 48;
+  const minfontsize = 12;
+  let fontsize = 16;
+  //funcion para darle alto contraste al body
+  contrastButton.onclick = function highContrast() {
+    body.classList.toggle("high-contrast");
+  };
+  //funcion de aumento de tamaño del texto
+  increaseFontButton.onclick = function increaseFont() {
+    if (fontsize < maxfontsize) {
+      fontsize = fontsize + 2;
+      body.style.fontSize = fontsize + "px";
+    }
+  };
+  //funcion de reduccion de tamaño del texto
+  decreaseFontButton.onclick = function decreaseFont() {
+    if (fontsize > minfontsize) {
+      fontsize = fontsize - 2;
+      body.style.fontSize = fontsize + "px";
+    };
+  };
 });
